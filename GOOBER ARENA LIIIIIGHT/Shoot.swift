24ae92d142dcabfcee75d7b4158d1ece9e0b7221
SPONG <Spongeislifesix@gmail.com>
2025-05-14 14:27:28 -0500

    qazxswedcvfrtgbnhyujmkiolp

--- GOOBER ARENA LIIIIIGHT/Shoot.swift
+++ GOOBER ARENA LIIIIIGHT/Shoot.swift
@@ -2,13 +2,11 @@ import SpriteKit
 import GameplayKit
 
 class Shoot: SKSpriteNode {
-    
     var owner: String = ""
-    
+
     func shootBullet(from ship: SKSpriteNode, in scene: SKScene, at location: CGPoint, pOwner: String, pSprite: String) -> Shoot {
         let bullet = Shoot(imageNamed: pSprite)
         bullet.zPosition = -5
-        
         bullet.owner = pOwner
 
         bullet.position = ship.position
@@ -27,7 +25,6 @@ class Shoot: SKSpriteNode {
         bullet.physicsBody?.isDynamic = false
 
         scene.addChild(bullet)
-        
         return bullet
     }
 }
